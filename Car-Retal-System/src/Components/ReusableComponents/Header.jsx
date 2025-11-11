10725777c46d55de816f56da8be463b4fb8f71c5
Kashiif-Khan <kashifafridi243@gmail.com>
2025-11-11 12:37:04 +0500

    new changes in structure

---
+++ Car-Retal-System/src/Components/ReusableComponents/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+        <>
+        <h1>Header Component</h1>
+        </>
+
+    )
+}
+
+export default Header
---
+++ Car-Retal-System/src/Components/Vehicles.jsx
@@ -0,0 +1,8 @@
+function Vehicles(){
+    return(
+        <>
+        <h1>Vehicals Page</h1>
+        </>
+    )
+}
+export default Vehicles
