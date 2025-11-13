a21d820677c6a6c0293c5c2c80fb85913dda4793
harisshah133560 <harisshah133560@gmail.com>
2025-11-13 07:43:23 -0800

    logocomp add

--- Car-Retal-System/src/Components/Vehicles.jsx
+++ Car-Retal-System/src/Components/Vehicles.jsx
@@ -5,6 +5,8 @@ import Mercedes from "../assets/Vehicallogo/Mercedes.png";
 import Jeep from "../assets/Vehicallogo/Jeep.png";
 import BMW from "../assets/Vehicallogo/BMW.png";
 import Audi from "../assets/Vehicallogo/Audi.png";
+import Card from "./ReusableComponents/card";
+import logocomp from "./ReusableComponents/logocomp";
 
 function Vehicles() {
   return (
@@ -39,46 +41,24 @@ function Vehicles() {
             Minivan
           </button>
         </div>
+        {/* Cards Section */}
+        <Card />
 
         {/* Logos Section */}
-        <div className="flex justify-center items-center py-10 px-4 h-40 mt-10 bg-gray-100">
-          <div className="bg-white shadow-md rounded-full px-10 py-6 flex flex-wrap justify-center items-center gap-12 w-fit">
-            <img
-              src={Toyota}
-              alt="Toyota"
-              className="h-10 w-auto object-contain hover:scale-110 transition-transform duration-300"
-            />
-            <img
-              src={Ford}
-              alt="Ford"
-              className="h-10 w-auto object-contain hover:scale-110 transition-transform duration-300"
-            />
-            <img
-              src={Mercedes}
-              alt="Mercedes"
-              className="h-10 w-auto object-contain hover:scale-110 transition-transform duration-300"
-            />
-            <img
-              src={Jeep}
-              alt="Jeep"
-              className="h-10 w-auto object-contain hover:scale-110 transition-transform duration-300"
-            />
-            <img
-              src={BMW}
-              alt="BMW"
-              className="h-10 w-auto object-contain hover:scale-110 transition-transform duration-300"
-            />
-            <img
-              src={Audi}
-              alt="Audi"
-              className="h-10 w-auto object-contain hover:scale-110 transition-transform duration-300"
-            />
-          </div>
-        </div>
-
-      </div>
-
-      <Footer />
+        <logocomp />
+          {/* <div className="flex justify-center items-center py-10 px-4 mt-10 bg-gray-100">
+            <div className="bg-white shadow-md rounded-full px-10 py-6 flex flex-wrap justify-center items-center gap-12 w-fit">
+              {[Toyota, Ford, Mercedes, Jeep, BMW, Audi].map((logo, i) => (
+                <img
+                  key={i}
+                  src={logo}
+                  alt={`Brand ${i + 1}`}
+                  className="h-10 w-auto object-contain hover:scale-110 transition-transform duration-300"
+                />
+              ))}
+            </div>
+          </div> */}
+      </div>    
     </>
   );
 }
